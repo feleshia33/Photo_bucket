dc4a17b070d53b36fe33987b388f285c16573254
feleshia33 <feleshiawest@gmail.com>
2022-07-30 20:05:54 -0500

    added toggle

--- React_app/react_proj/src/App.js
+++ React_app/react_proj/src/App.js
@@ -9,6 +9,13 @@ import './App.css';
 function App() {
   const [ toDoList, setToDoList ] = useState(data);
  
+  const handleToggle = (id) => {
+    let mapped = toDoList.map( task => {
+      return task.id == id ? {...task, complete: !task.complete } : {...task};
+    });
+    setToDoList(mapped);
+  }
+
  
  return (
    <div className="App">
