52d14864f444fa3ebf10c4e2e6a8a344daff3da4
feleshia33 <feleshiawest@gmail.com>
2022-08-05 15:36:39 -0500

    JS todo list

---
+++ JS_todo/todo.js
@@ -0,0 +1,46 @@
+//To strike thru when X is clicked --all lines
+// var str = document.getElementById("input-two");
+//     var result = str.strike();
+
+// document.getElementById("button-two").onclick = function(){
+//   document.getElementById("input-two").innerHTML = result;
+// }
+
+
+//To delete when X is clicked --all lines
+document.getElementById("button-one").onclick = function(){
+    document.getElementById("para-one").innerHTML = "";
+  }
+  document.getElementById("button-two").onclick = function(){
+    document.getElementById("para-two").innerHTML = "";
+  }
+  document.getElementById("button-three").onclick = function(){
+    document.getElementById("para-three").innerHTML = "";
+  }
+  document.getElementById("button-four").onclick = function(){
+    document.getElementById("para-four").innerHTML = "";
+  }
+  document.getElementById("button-five").onclick = function(){
+    document.getElementById("para-five").innerHTML = "";
+  }
+  document.getElementById("button-six").onclick = function(){
+    document.getElementById("para-six").innerHTML = "";
+  }
+  document.getElementById("button-seven").onclick = function(){
+    document.getElementById("para-seven").innerHTML = "";
+  }
+  document.getElementById("button-eight").onclick = function(){
+    document.getElementById("para-eight").innerHTML = "";
+  }
+  document.getElementById("button-nine").onclick = function(){
+    document.getElementById("para-nine").innerHTML = "";
+  }
+  document.getElementById("button-ten").onclick = function(){
+    document.getElementById("para-ten").innerHTML = "";
+  }
+  
+  
+  
+  
+  
+  
