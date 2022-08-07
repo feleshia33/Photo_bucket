9b0c93749e3e4adf568e51ecadccd55a4dbb278b
feleshia33 <feleshiawest@gmail.com>
2022-08-07 07:00:43 -0500

    to do list updates

--- todo_list/todo_list/src/App.js
+++ todo_list/todo_list/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import React, {useState} from 'react';
 import Title from './Title';
-// import todo from './todo';
+import todo from './todo';
 import toDolist from './ToDoList';
 
 function App() {
 
-  // const [toDoList, setToDo] = useState(todo);
+  const [toDoList, setToDo] = useState(todo);
 
 
   return (
     <div className="App">
       <Title />
-      {/* <ToDoList toDoList={todo}/> */}
+      <ToDoList toDoList={todo}/>
     </div>
   );
 }
--- todo_list/todo_list/src/ToDoList.js
+++ todo_list/todo_list/src/ToDoList.js
@@ -1,18 +1,18 @@
-// import React from 'react';
-// // import ToDo from './ToDo';
+import React from 'react';
+// import ToDo from './ToDo';
 
 
-// const ToDoList = ({todos}) => {
-//     return (
-//         <div>
-//             {todos.map(todos => {
-//                 return (
-//                     <ToDo todos={todos} />
-//                 )
-//             })}
-//         </div>
+const ToDoList = ({todos}) => {
+    return (
+        <div>
+            {todos.map(todos => {
+                return (
+                    <ToDo todos={todos} />
+                )
+            })}
+        </div>
 
-//     );
-// };  
+    );
+};  
 
-// export default ToDoList;
+export default ToDoList;
