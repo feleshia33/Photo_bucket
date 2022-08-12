f151301330214d1f6b8e137cd709704071b493a2
feleshia33 <feleshiawest@gmail.com>
2022-08-12 14:19:29 -0500

    added api key

--- recipe/recipe/src/App.js
+++ recipe/recipe/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = '917df091';
+  const APP_KEY = '7d5e6336dc34654486609295d5c92499';
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return(
     <div className="App">
-      
+      <form className="search-form">
+        <input className="search-type" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
