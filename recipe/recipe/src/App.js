f07981eec24927dadc3bbdb004c5d9cbcd15e175
feleshia33 <feleshiawest@gmail.com>
2022-08-12 14:35:33 -0500

    added search and button

--- recipe/recipe/src/App.js
+++ recipe/recipe/src/App.js
@@ -1,3 +1,5 @@
+
+import React, {useEffect, useState}from "react";
 import './App.css';
 
 const App = () => {
@@ -7,12 +9,21 @@ const App = () => {
 
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run.');
+  });
+
+
+
   return(
     <div className="App">
       <form className="search-form">
         <input className="search-type" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
+      <h1 onClick = {() => setCounter(counter + 1 )}>{counter}</h1>
     </div>
   );
 }
