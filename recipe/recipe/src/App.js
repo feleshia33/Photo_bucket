a81e88d39eddabbadde9be4490bf494342bfe48d
feleshia33 <feleshiawest@gmail.com>
2022-08-15 17:58:39 -0500

    added form, label, nutri info

--- recipe/recipe/src/App.js
+++ recipe/recipe/src/App.js
@@ -1,20 +1,25 @@
 
 import React, {useEffect, useState}from "react";
+import Recipe from './Recipe'
 import './App.css';
+
 
 const App = () => {
 
   const APP_ID = '917df091';
   const APP_KEY = '7d5e6336dc34654486609295d5c92499';
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-  const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    console.log('Effect has been run.');
-  });
+    getRecipes();
+  }, []);
 
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
 
 
   return(
@@ -23,7 +28,9 @@ const App = () => {
         <input className="search-type" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
-      <h1 onClick = {() => setCounter(counter + 1 )}>{counter}</h1>
+      {recipes.map(recipe => (
+        <Recipe title={recipe.recipe.label} calories={receipe.receipe.calories}}/>
+      ))};
     </div>
   );
 }
