41511e65a44483f097a5b6512c971e73c9b2cb28
feleshia33 <feleshiawest@gmail.com>
2022-08-16 16:34:55 -0500

    add title calories image

--- recipe/recipe/src/App.js
+++ recipe/recipe/src/App.js
@@ -19,21 +19,21 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-  }
+    console.log(data.hits);
+  };
 
 
   return(
     <div className="App">
       <form className="search-form">
-        <input className="search-type" type="text" />
+        <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
       {recipes.map(recipe => (
         <Recipe 
         title={recipe.recipe.label} 
-        calories={recipe.receipe.calories}
+        calories={recipe.recipe.calories}
         image={recipe.recipe.image}
-        
         />
       ))}
     </div>
