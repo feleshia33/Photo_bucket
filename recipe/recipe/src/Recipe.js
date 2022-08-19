95181c4c051eba3aa289ced502b776d63f672c5a
feleshia33 <feleshiawest@gmail.com>
2022-08-18 23:41:59 -0500

    updates

--- recipe/recipe/src/Recipe.js
+++ recipe/recipe/src/Recipe.js
@@ -5,11 +5,11 @@ const Recipe = ({title, calories, image, ingredients}) => {
     return (
         <div>
             <h1>{title}</h1>
-            <ol>    
+            <ul>    
                 {ingredients.map(ingredients => (
                     <li>{ingredients.text}</li>
                 ))}
-            </ol>
+            </ul>
             <p>{calories}</p>
             <img src={image} alt="" />
         </div>
