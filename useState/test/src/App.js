ae190eba2bfa72dbb7996215057c8e5f829484e8
feleshia33 <feleshiawest@gmail.com>
2022-08-23 15:08:16 -0500

    practice useState

--- useState/test/src/App.js
+++ useState/test/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(4) //count is current iteration, setCount is what ever state is set next
   return (
     <div className='app'>
     <button>-</button>
