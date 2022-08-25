a602bec7fe91e110821ed1a23615cc2d44f33ba1
feleshia33 <feleshiawest@gmail.com>
2022-08-24 23:41:02 -0500

    inital commit

---
+++ photos/photo_bucket/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header.js';
+import Search from './Search';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
