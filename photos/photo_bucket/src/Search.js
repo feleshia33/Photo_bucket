a1898f7307848c122626c666b32dc217d8f70be0
feleshia33 <feleshiawest@gmail.com>
2022-08-25 00:06:33 -0500

    added propTypes

--- photos/photo_bucket/src/Search.js
+++ photos/photo_bucket/src/Search.js
@@ -1,14 +1,20 @@
-const Search = (props) => {
+import PropTypes from 'prop-types' //impr to import prop types
+
+const Search = ({query}) => {
     return (
      <div>
-          <input type="text" placeholder={props.query}/>
+          <input type="text" placeholder={query}/>
           <button>Search</button>
      </div>
     )
   }
 
   Search.defaultProps = {
-       query: 'Search for photo image'
+       query: 'Search for photo image',
+  }
+
+  Search.propTypes = {
+       query: PropTypes.string.isRequired,
   }
   
   export default Search;
