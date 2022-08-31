31eb2ddfe4ba220cc4715ab212907965c6b411d5
feleshia33 <feleshiawest@gmail.com>
2022-08-31 14:17:30 -0500

    added key in image array

--- photos/photo_bucket/src/Images.js
+++ photos/photo_bucket/src/Images.js
@@ -1,30 +1,29 @@
 //import React from 'react' rac shortcut
 import image_one from './Images'
 
-const images = [
-{
-    id: 1,
-    image: 'image_one.png',
-    text: 'Ispum lalala fee'
-    
-},
-{
-    id: 2,
-    image: 'image_two.png',
-    text: 'Ispum lalala feed'
-}
+function Images() {
+    const images = [
+    {   key: 1, 
+        image: 'image_one.png', 
+        text: 'Ispum lalala fee'
+    },
+    {
+        key: 2, 
+        image: 'image_two.png', 
+        text: 'Ispum lalala feed'
+    }   
+    ];
 
-]
+// const Images = () => {
+    return (
+        <>
+            {images.map(({ key, image }) => (
+                <h3 key={key}>{image.image}</h3>
 
-const Images = () => {
-  return (
-    <>
-    {images.map(({key, image}) => (
-    <h3>{image.image}</h3>
-    
-    ))}
-    </>
-  )
+            ))}
+        </>
+    );
 }
+//}
 
 export default Images
