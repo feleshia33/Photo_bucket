792fc08c9d0a7181bc4a905f99a3c715504dcca3
feleshia33 <feleshiawest@gmail.com>
2022-08-31 13:39:23 -0500

    images import pending

--- photos/photo_bucket/src/Images.js
+++ photos/photo_bucket/src/Images.js
@@ -1,17 +1,17 @@
 //import React from 'react' rac shortcut
-//import image_one.png from './Images'
+import image_one from './Images/image_one.png'
 
 const images = [
 {
     id: 1,
-    image: './Images/image_one.png',
-    text: ''
+    image: 'image_one.png',
+    text: 'Ispum lalala fee'
     
 },
 {
     id: 2,
-    image: './Images.image_two.png',
-    text: ''
+    image: 'image_two.png',
+    text: 'Ispum lalala feed'
 },
 {
     id: 3,
@@ -22,35 +22,8 @@ const images = [
     id: 4,
     image: '',
     text: ''
-},
-{
-    id: 5,
-    image: '',
-    text: ''
-},
-{
-    id: 6,
-    image: '',
-    text: ''
-},
-{
-    id: 7,
-    image: '',
-    text: ''
-},
-{
-    id: 8,
-    image: '',
-    text: ''
-},{
-    id: 9,
-    image: '',
-    text: ''
-},{
-    id: 10,
-    image: '',
-    text: ''
-},
+}
+
 ]
 
 const Images = () => {
